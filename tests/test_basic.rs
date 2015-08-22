6fe732b6fb7f0a5669c67461cf4f7b0bef4cf63f
Sebastian Waisbrot <seppo0010@gmail.com>
2015-08-22 10:14:56 -0700

    Extract tests

---
+++ tests/test_basic.rs
@@ -0,0 +1,38 @@
+extern crate ohmers;
+extern crate redis;
+extern crate rustc_serialize;
+
+use ohmers::{get, Ohmer};
+use rustc_serialize::Encodable;
+
+#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
+struct Person {
+    id: usize,
+    name: String,
+}
+impl Ohmer for Person {
+    fn id(&self) -> usize { self.id }
+    fn set_id(&mut self, id: usize) { self.id = id; }
+    fn defaults() -> Self {
+        Person {
+            id: 0,
+            name: "".to_string(),
+        }
+    }
+}
+
+#[test]
+fn test_save() {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut person = Person { id: 0, name: "John".to_string() };
+    person.save(&client).unwrap();
+}
+
+#[test]
+fn test_save_load() {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut person = Person { id: 0, name: "Jane".to_string() };
+    person.save(&client).unwrap();
+    let person2 = get(person.id, &client).unwrap();
+    assert_eq!(person, person2);
+}
